0b4122c7b884d528c5f612691993c5551abaa24a
Lukasz Bartosz <bartoszlukasz@gmail.com>
2016-01-17 23:38:40 +0100

    Adding basic motor control functions

--- FW/bb8_fw/bb8_fw.ino
+++ FW/bb8_fw/bb8_fw.ino
@@ -6,6 +6,9 @@
 
 #include <servo.h>
 
+//==================================
+// enable/disable test mode here
+#define TEST_ENABLED 0
 
 // =================================
 // pin assignments here and other constants
@@ -21,19 +24,38 @@
 
 #define PIN_M2_SPEED  12	// D6 PD6
 #define PIN_M2_LEFT 14		// PC0 ADC0
-#define PIN_M2_RIGHT 15		// PC1ADC1
+#define PIN_M2_RIGHT 15		// PC1 ADC1
 
 #define PIN_PIEZO 11		// D12 PB3 MOSI
+
+#define WAIT 1000
+#define MIN_MOTOR_SPEED 150
+#define MAX_MOTOR_SPEED 255
 
 
 // globals
 Servo servo_head_x;
 Servo servo_head_y;
-#define WAIT 1000
+
+int translation = 0;
+int rotation = 0;
+int m1_speed = 0;
+int m1_mode = 0;
+int m2_speed = 0;
+int m2_mode = 0;
 
 
 
-// the setup function runs once when you press reset or power the board
+
+//==================================
+// functions declarations
+void test_actuators(void);
+void move_motor_1(bool, int);
+void move_motor_2(bool, int);
+void full_stop(bool, bool);
+
+//==================================
+// SYSTEM SETUP
 void setup() {
 	// head control servos
 	servo_head_x.attach(PIN_HEAD_SERVO_X, SERVO_MIN, SERVO_MAX);
@@ -47,50 +69,137 @@ void setup() {
 	pinMode(PIN_PIEZO, OUTPUT);
 }
 
-// the loop function runs over and over again until power down or reset
+//==================================
+// MAIN LOOP
 void loop() {
-
-	//piezo test
-	if(1) {
-		for (int i = 500; i >= 0; i--) {
-			digitalWrite(PIN_PIEZO, LOW);
-			delayMicroseconds(1000);
-			digitalWrite(PIN_PIEZO, HIGH);
-			delayMicroseconds(1000);
-		}
-		digitalWrite(PIN_PIEZO, LOW);
-		delay(WAIT);
+	
+	// test code to check actuators
+	while(TEST_ENABLED) {
+		test_actuators();
 	}
 	
-	//serwo test
-	if (1) {
-		servo_head_x.write(0);
-		delay(WAIT);
-		servo_head_x.write(180);
-		delay(WAIT);
+	// real control code
 
-		servo_head_y.write(0);
-		delay(WAIT);
-		servo_head_y.write(180);
-		delay(WAIT);
-	}
+	// get cmd via bt
+	int ctrls_received = receive_control_via_bt();
 
-	//motor test
-	if (true) {
-		//analogWrite(PIN_M1_SPEED, 0);
+	// some ifs here to read what to do from ctrl commands
+
+	// call move functions
+	
+	
+
+}
+
+//==================================
+// function definitions
+
+int receive_control_via_bt(void) {
+	// get and return bt control transmission here
+	return 0;
+}
+
+void move_motor_1(bool forward = true, int target_speed = MAX_MOTOR_SPEED) {
+	// accelerate and start rolling forward
+	// return current engine speed delta
+
+	if (forward) {
 		digitalWrite(PIN_M1_LEFT, HIGH);
 		digitalWrite(PIN_M1_RIGHT, LOW);
-		for (int speed = 150; speed <= 255; speed++) {
-			analogWrite(PIN_M1_SPEED, speed);
-			delay(10);
-		}
-		delay(3000);
-		for (int speed = 255; speed <= 150; speed--) {
-			analogWrite(PIN_M1_SPEED, speed);
-			delay(10);
-		}
-		analogWrite(PIN_M1_SPEED, 0);
-		delay(WAIT);
+	}
+	else {
+		digitalWrite(PIN_M1_LEFT, LOW);
+		digitalWrite(PIN_M1_RIGHT, HIGH);
+	}
+	
+
+	for (int speed = max(m1_speed, MIN_MOTOR_SPEED); speed <= target_speed; speed++) {
+		analogWrite(PIN_M1_SPEED, speed);
+		delay(10);
+	}
+	m1_speed = target_speed;
+}
+
+void move_motor_2(bool forward = true, int target_speed = MAX_MOTOR_SPEED) {
+	// accelerate and start rolling forward
+	// return current engine speed delta
+
+	if (forward) {
+		digitalWrite(PIN_M2_LEFT, HIGH);
+		digitalWrite(PIN_M2_RIGHT, LOW);
+	}
+	else {
+		digitalWrite(PIN_M2_LEFT, LOW);
+		digitalWrite(PIN_M2_RIGHT, HIGH);
 	}
 
+
+	for (int speed = max(m2_speed, MIN_MOTOR_SPEED); speed <= target_speed; speed++) {
+		analogWrite(PIN_M2_SPEED, speed);
+		delay(10);
+	}
+	m2_speed = target_speed;
+}
+
+
+void full_stop(bool stop_m1 = true, bool stop_m2 = true) {
+	if (stop_m1) {
+		digitalWrite(PIN_M1_LEFT, LOW);
+		digitalWrite(PIN_M1_RIGHT, LOW);
+		analogWrite(PIN_M1_SPEED, 0);
+		m1_speed = 0;
+	}
+	if (stop_m2) {
+		digitalWrite(PIN_M2_LEFT, LOW);
+		digitalWrite(PIN_M2_RIGHT, LOW);
+		analogWrite(PIN_M2_SPEED, 0);
+		m2_speed = 0;
+	}
+	// wait for bb8 to stop swaying
+	delay(200);
+}
+
+void test_actuators(void) {
+
+//piezo test
+	for (int i = 500; i >= 0; i--) {
+		digitalWrite(PIN_PIEZO, LOW);
+		delayMicroseconds(1000);
+		digitalWrite(PIN_PIEZO, HIGH);
+		delayMicroseconds(1000);
+	}
+	digitalWrite(PIN_PIEZO, LOW);
+	delay(WAIT);
+	
+	
+	//serwo test
+	servo_head_x.write(0);
+	delay(WAIT);
+	servo_head_x.write(180);
+	delay(WAIT);
+
+	servo_head_y.write(0);
+	delay(WAIT);
+	servo_head_y.write(180);
+	delay(WAIT);
+	
+
+	//motor test
+	digitalWrite(PIN_M1_LEFT, HIGH);
+	digitalWrite(PIN_M1_RIGHT, LOW);
+	for (int speed = 150; speed <= 255; speed++) {
+		analogWrite(PIN_M1_SPEED, speed);
+		delay(10);
+	}
+	delay(3000);
+	for (int speed = 255; speed <= 150; speed--) {
+		analogWrite(PIN_M1_SPEED, speed);
+		delay(10);
+	}
+	analogWrite(PIN_M1_SPEED, 0);
+	delay(WAIT);
+}
+
+void move_motor_2(void)
+{
 }
